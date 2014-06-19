ebf869d998825774f188d2668312f7034216d748
Jasper den Ouden <o.jasper@gmail.com>
2014-06-19 02:52:25 +0200

    Placeholder for adding the ethereum "middle-end" later

---
+++ javascripts/bitvote_ethereum.js
@@ -0,0 +1,17 @@
+//Api to Ethereum backend.
+
+// (something like eth.getStorageAt(vote_address, own_address))
+//function from_time()
+
+// something like
+// eth.transact(_sec, 0, vote_address, bin(vote_for), 1000, 1, complete_spend(vote_for));
+//function do_spend_time(vote_for, amount)
+
+//Will have to parse the current pending transactions, and keep state somehow?
+//var participated = {};
+
+//Address of the bitvote Ethereum contract.(might be a bunch of contracts)
+//var vote_address = "TODO";
+
+//secretToAddress(_a):
+//var own_address = "TODO"; 
