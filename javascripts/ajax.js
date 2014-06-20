d3cc24bc0f9d3012d9bd2903ec3618f7cc610745
Ethan <ethan@coinculture.info>
2014-06-20 15:46:36 +0000

    cleaner ajax.js with general purpose ajax calls

--- javascripts/ajax.js
+++ javascripts/ajax.js
@@ -1,3 +1,4 @@
+	  //echo "$time_str <a href='http://$url'>http://$url</a>\n";
 
 function new_request() {
     if (window.XMLHttpRequest)
@@ -6,37 +7,40 @@ function new_request() {
         return new ActiveXObject("Microsoft.XMLHTTP");
 }
 
-function lookup_user(usr_id, cur_time){
-    xmlhttp = new_request();
+function register_callback(xmlhttp, _func){
     xmlhttp.onreadystatechange=function(){
         if (xmlhttp.readyState==4 && xmlhttp.status==200){
-    	    usr_details = JSON.parse(xmlhttp.responseText);
-	    _lookup_user(usr_details);
+		var res = JSON.parse(xmlhttp.responseText);
+		_func(res);
         }
     }
-    var query_string = "usr_id="+encodeURIComponent(usr_id)+"&cur_time="+encodeURIComponent(cur_time);
-    xmlhttp.open("POST", "ajax/lookup_user.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.setRequestHeader("Content-length", query_string.length);
-    xmlhttp.send(query_string);
-    return false;
+}
 
+function make_request(xmlhttp, method, path, async, params){
+    xmlhttp.open(method, path, async);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var s = "", i = 0;
+    for (var k in params){
+        if (i > 0)
+          s += "&";
+        s += k+"="+encodeURIComponent(params[k]);
+        i++;
+    }
+    //xmlhttp.setRequestHeader("Content-length", s.length);
+    xmlhttp.send(s);
+}
+
+
+function lookup_user(usr_id, cur_time){
+    xmlhttp = new_request();
+    register_callback(xmlhttp, _lookup_user);
+    make_request(xmlhttp, "POST", 'ajax/lookup_user.php', true, {"usr_id":usr_id, "cur_time":cur_time});
+    return false;
 }
 
 function cast_vote(url, spent, usr_id){
     xmlhttp = new_request();
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-		console.log(xmlhttp.responseText);
-		response = JSON.parse(xmlhttp.responseText);
-		_cast_vote(response);
-		
-        }
-    }
-    var query_string = "url="+encodeURIComponent(url)+"&usr_id="+encodeURIComponent(usr_id)+"&time_spent="+encodeURIComponent(spent)+"&time_reg="+encodeURIComponent(reg_time);
-    xmlhttp.open("POST", "ajax/cast_vote.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.setRequestHeader("Content-length", query_string.length);
-    xmlhttp.send(query_string);
+    register_callback(xmlhttp, _cast_vote);
+    make_request(xmlhttp, "POST", 'ajax/cast_vote.php', true, {"url":url, "usr_id":usr_id, "time_spent":spent, "time_reg":reg_time});
     return false;
 }
