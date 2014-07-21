d12ae08ac0708897b2e69418274c03209c03638b
Jasper den Ouden <o.jasper@gmail.com>
2014-07-21 18:06:13 +0200

    Backendless back up.. not very good..

---
+++ javascripts/bitvote_really_no_backend.js
@@ -0,0 +1,12 @@
+function lookup_user(usr_id, cur_time) {
+    time = (new Date()).getTime()/1000
+    _lookup_user({'spent':0, 'reg':time})
+}
+
+function get_user_ip() {
+    return "just_demo_got_nothing"  // Server version has it, but still demo!
+}
+
+function cast_vote(spend_addr, spend_time, from_user) {
+    
+}
