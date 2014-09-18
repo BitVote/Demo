80b6082951b4d6b881bce9bac5bdef7fe8a64701
Jasper den Ouden <o.jasper@gmail.com>
2014-09-18 18:40:53 +0200

    Adding topics test

--- eth-entity/approach2/cases.py
+++ eth-entity/approach2/cases.py
@@ -27,19 +27,21 @@ def ae(a, b, cond=None, what="N/A"):
         print('-', map(stri,a), "vs", map(stri,b), ":", what)        
         assert False
 
-def store(index):
-    result = int(s.block.get_storage_data(c, index))
-    ae(s.send(t.k9, c, 0, [i("account"), index]), [result], None, "store mismatch")
+def store(index, contract=c):
+    result = int(s.block.get_storage_data(contract, index))
+    if contract == c:
+        ae(s.send(t.k9, contract, 0, [i("account"), index]), [result],
+           None, "store mismatch")
     return result
 
-def i_store(index):
-    return int(store(index))
+def i_store(index, contract=c):
+    return int(store(index, contract))
 
-def str_store(index):
-    return stri(store(index))
+def str_store(index, contract=c):
+    return stri(store(index, contract))
 
-def addr_store(index):
-    return hex(store(index))[2:-1]
+def addr_store(index, contract=c):
+    return hex(store(index, contract))[2:-1]
 
 def reset():
     global c,s
@@ -80,9 +82,13 @@ def check():  # TODO this would be better with 'stateless call'
     ae(s.send(t.k9, c, 0, [1, 2, 3]), [i("anyone bad 2")])
     ae(s.send(t.k9, c, 0, []), [i("anyone bad 3")])
 
+    # Ways you cannot set the one per ID.
     ae(s.send(t.k0, c, 0, []), [i("OnePerIDSet bad")])
     ae(s.send(t.k0, c, 0, [randrange(LARGE)]), [i("OnePerIDSet bad")])
 
+def no_topics_yet():
+    non_exist_vote_count(0)
+    non_exist_vote(0)
 
 def start():
     reset()
@@ -90,8 +96,44 @@ def start():
     ae(i_store(0x00), 0)
     assert addr_store(0x20) == t.a0
     ae(i_store(0x40), 0x60)
-    
-    non_exist_vote_count(0)
-    non_exist_vote(0)
+    no_topics_yet()
 
-start()
+c2 = s.contract('any_per_id.se', t.k0)  # TODO test of that alone.
+
+def initialize():
+    start()
+    c2 = s.contract('any_per_id.se', t.k0)
+    assert addr_store(0, c2) == t.a0
+    # Gives himself full power, the bastard.
+    ae(s.send(t.k0, c, 0, [c2, t.a0]), [i("changed!")])
+    assert addr_store(0x00) == c2 and addr_store(0x20) == t.a0
+    check()
+    for x in [[1,2], []]:
+        ae(s.send(t.k0, c2, 0, x), [i("initializer bad")])
+    ae(s.send(t.k0, c2, 0, [c]), [i("initialized")])
+    assert i_store(0, c2) == 0
+    no_topics_yet()
+
+def add_topic(string=None):
+    if string == None:
+        string = ""
+        for j in range(randrange(20)):
+            string += ["herp", "derp", "blurb", "bla"][randrange(4)]
+    args = []
+    while string != "":
+        args.append(i(string[:32]))
+        string = string[32:]
+    
+    j = i_store(0x40)
+    if len(args) > 224 - 0x20:
+        ae(s.send(t.k2, c, 0, args), ["too long topic string"])
+        assert i_store(0x40) == j  # Cant have added it anyway.
+        check()
+    elif len(args) > 3:
+        ae(s.send(t.k2, c, 0, args), ["topic set"])
+        assert i_store(0x40) == j + 224   # Must have indeed moved forwar.d
+        check()
+    #else: # Could be any of the other commands.
+
+initialize()
+add_topic()
