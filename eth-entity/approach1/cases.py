813ddc14607be78737e711fc6221c7f9af62538a
Jasper den Ouden <o.jasper@gmail.com>
2014-09-30 17:17:25 +0200

    Yep, more accidentally-uppercase problems

--- eth-entity/approach1/cases.py
+++ eth-entity/approach1/cases.py
@@ -25,8 +25,8 @@ c2 = s.contract('any_per_id.se', t.k0)  # TODO test of that alone.
 
 def ae(a, b, cond=None, what="N/A"):
     if (a !=b if cond == None else not cond):
+        print('-', map(stri,a), "vs", map(stri,b), ":", what)
         print(map(hex,a), "vs", map(hex,b), ":", what)
-        print('-', map(stri,a), "vs", map(stri,b), ":", what)        
         assert False
 
 def store(index, contract=None):
@@ -36,7 +36,7 @@ def store(index, contract=None):
     result = int(s.block.get_storage_data(contract, index))
     if contract == c:
         ae(s.send(t.k9, contract, 0, [i("account"), index]), [result],
-           None, "store mismatch")
+           None, what="store mismatch")
     return result
 
 def i_store(index, contract=None):
@@ -49,26 +49,29 @@ def addr_store(index, contract=None):
     return hex(store(index, contract))[2:-1]
 
 LARGE = 1152921504606846976
+HALFWAY = 340282366920938463463374607431768211456
 
 def non_exist_vote_count(j=None):
     if not j:
         j = (i_store(0x40) - 0x60)/224
     ae(s.send(t.k9, c, 0, [i("vote_count"), j]),
-        [i("That topic doesnt exist yet.")],
+        [i("topic doesnt exist yet.")],
         "getting vote count nonexistance failed.")
 
 def non_exist_vote(j=None):
     if not j:
         j = (i_store(0x40) - 0x60)/224
     ae(s.send(t.k9, c, 0, [i("vote"), j, randrange(0,10)]),
-       [i("That topic doesnt exist yet.")])
+       [i("topic doesnt exist yet(vote)")])
 
-def expect_topic_count(n):
+def expect_topic_count(n, zeros=True):
     ae([i_store(0x40)], [0x60 + 224*n])
     ae(s.send(t.k9, c, 0, [i("topic_count")]), [n])
     non_exist_vote(randrange(n, n+3))
     non_exist_vote_count(randrange(n, n+3))
-    
+    if n > 0:
+        ae(s.send(t.k9, c, 0, [i("vote_count"), 0]), [0],
+           what="Not zero votes on it/wrong message.")
 
 def too_long_topic():
     ae(s.send(t.k9, c, 0, map(lambda i : i, range(224 + randrange(1,10)))),
@@ -78,8 +81,8 @@ def check():  # TODO this would be better with 'stateless call'
     # Smaller than large.
     ae(i_store(0x40), LARGE, i_store(0x40) < LARGE, "topic index unrealistic")
     # Thing that can happen in any case.
-    non_exist_vote()
-    non_exist_vote_count()
+    non_exist_vote(store(0x40))
+    non_exist_vote_count(store(0x40))
     too_long_topic()
     ae(s.send(t.k9, c, 0, [1, 2]), [i("anyone bad 1")])
     ae(s.send(t.k9, c, 0, [1, 2, 3]), [i("anyone bad 2")])
@@ -95,10 +98,16 @@ def no_topics_yet():
     non_exist_vote(0)
     expect_topic_count(0)
 
+run_i = 0
 
 def scenario_start():
-    global c
+    s.mine()  # Otherwise a series of tests could hit the block gas limit.
+    global c, c2, run_i
     c = s.contract('bitvote.se', t.k0)
+    c2 = s.contract('any_per_id.se', t.k0)
+    run_i = run_i + 1
+    print("Run " + str(run_i) + " bitvote: " + c + " anyperid: " + c2)
+    
     check()
     ae(i_store(0x00), 0)
     assert addr_store(0x20) == t.a0
@@ -106,14 +115,12 @@ def scenario_start():
     no_topics_yet()
 
 def initialize(have_topics=False):
-    global c2
-    c2 = s.contract('any_per_id.se', t.k0)
     # TODO check that it responds with "not initialized" when registering.
     assert addr_store(0, c2) == t.a0
     # Gives himself full power, the bastard.
     ae(s.send(t.k0, c, 0, [c2, t.a0]), [i("changed!")])
+    ae([store(0x00)], [int(c2,16)])
     assert addr_store(0x20) == t.a0
-    assert addr_store(0x00) == c2
     check()
     for x in [[1,2], []]:
         ae(s.send(t.k0, c2, 0, x), [i("initializer bad")])
@@ -142,7 +149,9 @@ def add_topic(string=None):
     elif len(args) > 3:
         ae(s.send(t.k2, c, 0, args), [i("topic set")])
         assert i_store(0x40) == j + 224   # Must have indeed moved forward.
-        #TODO check message itself.
+        assert i_store(j) == 0  # Must start with zero votes.
+        for k in range(len(args)):  # Check message.
+            assert i_store(j + 0x20 + k*0x20) == args[k]
     else:
         print("na", len(args))
     check()
@@ -166,16 +175,29 @@ def scenario_create_topics(init_first=False):
 def scenario_register():
     n = scenario_create_topics()
     ae(s.send(t.k2, c2, 0, []), [i("registered")])
+    # Check that timestamp set right.
+    tm = s.block.timestamp
+    assert tm + HALFWAY*tm == store(int(t.a2,16))
     # TODO check timestamp on the account.
     check()
-    return n
+    return n, tm
 
 def scenario_vote():
-    n = scenario_register()
+    n, tm = scenario_register()
     j = randrange(n)
-    ae(s.send(t.k2, c2, 0, [i("vote"), j, 60]), [i("cannot spend more than you have")])
+    if s.send(t.k2, c, 0, [i("vote"), j, 60]) != [i("cannot spend more than you have")]:
+        print(n,j, store(0x40), (store(0x40)-0x60)/224)
+    ae(s.send(t.k2, c, 0, [i("vote"), j, 60]), [i("cannot spend more than you have")])
+    assert tm + HALFWAY*tm == store(int(t.a2,16))
+    
     s.mine(100)  # Get some time. TODO test not independent on block time right now.
-    ae(s.send(t.k2, c2, 0, [i("vote"), j, 60]), [i("voted")]) # Vote 60s.
-    #TODO check it moved forward enough.
+    use_tm = randrange(60,600)
+    ae(s.send(t.k2, c, 0, [i("vote"), j, use_tm]), [i("voted")])
+    #print(tm, tm + use_tm, store(int(t.a2,16))/HALFWAY, store(int(t.a2,16))%HALFWAY)        
+    # Check that vote counter indeed advanced.
+    assert tm + HALFWAY*(tm + use_tm) == store(int(t.a2,16))
+    # Check that topic received said votes.
+    ae(s.send(t.k9, c, 0, [i("vote_count"), j]), [use_tm])
 
-scenario_register()
+for k in range(8):
+    scenario_vote()
